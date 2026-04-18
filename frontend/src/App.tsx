bcb7e3ff2a67c8bfb25c77f9240f1282c283dc67
Jaredsalaz <jared.salazarg@hotmail.com>
2026-04-17 23:47:05 -0600

    solucionando error de compilacion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import RegistrationForm from './components/RegistrationForm';
 import { SuccessScreen } from './components/OTPModal';
