2acac4996ff51c008ce3c5cefea8d6f590f10922
Jaredsalaz <jared.salazarg@hotmail.com>
2026-04-21 01:59:00 -0600

    fix: update API_BASE_URL to VPS IP

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE_URL = 'https://qsd.fly.dev/api';
+export const API_BASE_URL = 'http://51.89.23.206:8888/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
